b883ae4a80ceb937ac8b619edb24378f5e021709
TUVX <tuhaha900@gmail.com>
2022-02-26 15:11:10 +0700

    Created Check
    Never mind

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
-
+import sample from "./api/sample.route.js"
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/restaurants", restaurants)
+app.use("/api/v1/sample", sample)
+
 app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 
 export default app
