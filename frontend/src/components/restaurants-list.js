84d3982a38d04c1ac5017322242411997d2bbcfe
TUVX <tuhaha900@gmail.com>
2022-02-27 17:56:16 +0700

    Hello Word With Front-end

---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function RestaurantsList() {
+  return (
+    <div className="App">
+        RestaurantsList Page
+    </div>
+  );
+}
+
+export default RestaurantsList;
---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Restaurant() {
+  return (
+    <div className="App">
+        Single Restaurant
+    </div>
+  );
+}
+
+export default Restaurant;
