47615146c0b96cc22a4b1334b532efd9f3b97e9c
TUVX <tuhaha900@gmail.com>
2022-02-27 17:26:50 +0700

    Basic Back-end Finish

--- backend/api/sample.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-
-const router = express.Router()
-
-router.route("/").get((req,res)  => res.send("hello word"))
-
-
-export default router
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express"
 import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
-import sample from "./api/sample.route.js"
+//import sample from "./api/sample.route.js"
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/restaurants", restaurants)
-app.use("/api/v1/sample", sample)
+//app.use("/api/v1/sample", sample)
 
 app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 
