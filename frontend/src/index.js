7d7da4d26329c4e35d8fc5eda62421fc095520d4
TUVX <tuhaha900@gmail.com>
2022-03-07 15:37:51 +0700

    commit2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <BroswerRouter>
+  <BrowserRouter>
     <App />
-  </BroswerRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
